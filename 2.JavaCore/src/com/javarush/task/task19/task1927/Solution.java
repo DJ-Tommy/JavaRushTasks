3676bb275b4b7dbf70a0ee72929fc05c1437207f
NB <mymail@email.mail>
2019-03-19 18:43:18 +0300

    Update

--- 2.JavaCore/src/com/javarush/task/task19/task1927/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1927/Solution.java
@@ -4,10 +4,33 @@ package com.javarush.task.task19.task1927;
 Контекстная реклама
 */
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
+        PrintStream original = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        String text = outputStream.toString();
+        System.setOut(original);
+        ArrayList<String> list = new ArrayList<String>();
+        list.addAll(Arrays.asList(text.split("\\n")));
+        int count = 0;
+        for (String s : list) {
+            count++;
+            System.out.println(s);
+            if (count >= 2) {
+                System.out.println("JavaRush - курсы Java онлайн");
+                count = 0;
+            }
+        }
     }
 
     public static class TestString {
