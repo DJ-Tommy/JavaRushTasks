e01eff6620b50619fa4d7bd899b38516a234378d
DJ-Tommy <dj_tommy@rambler.ru>
2019-04-14 10:24:15 +0300

    New Update

--- 2.JavaCore/src/com/javarush/task/task20/task2022/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2022/Solution.java
@@ -6,26 +6,28 @@ import java.io.*;
 Переопределение сериализации в потоке
 */
 public class Solution implements Serializable, AutoCloseable {
-    private FileOutputStream stream;
+    private transient FileOutputStream stream;
+    private String fileName;
 
     public Solution(String fileName) throws FileNotFoundException {
         this.stream = new FileOutputStream(fileName);
+        this.fileName = fileName;
     }
 
     public void writeObject(String string) throws IOException {
         stream.write(string.getBytes());
-        stream.write("\n".getBytes());
+        stream.write("\r\n".getBytes());
         stream.flush();
     }
 
     private void writeObject(ObjectOutputStream out) throws IOException {
         out.defaultWriteObject();
-        out.close();
     }
 
     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
         in.defaultReadObject();
-        in.close();
+        stream = new FileOutputStream(fileName, true);
+
     }
 
     @Override
@@ -34,7 +36,17 @@ public class Solution implements Serializable, AutoCloseable {
         stream.close();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        String fileName = "d:\\2022.txt";
+        String fileName1 = "d:\\2022-1.txt";
+        Solution solution = new Solution(fileName);
+        solution.writeObject("String String String");
+        ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(fileName1));
+        outputStream.writeObject(solution);
+        outputStream.close();
+        ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(fileName1));
+        solution = (Solution) inputStream.readObject();
+        solution.writeObject("222222222");
 
     }
 }
