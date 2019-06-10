5736f0ede4d9d863b9ee38171092a1049671ce65
Nikolai Borisov <nborisov@ipmks.com>
2019-06-10 15:25:27 +0300

    Update milane

--- 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
@@ -11,8 +11,17 @@ public class Solution {
     public static long[][] DEGREE = new long[11][20];
     public static ArrayList<byte[]> listZZ = new ArrayList<>();
 
-
     static {
+        // Данный массив предназначен просто для информации обо всех числах армстронга и не используется в решении
+        long[] armstrongValues = new long[]{
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407, 1634, 8208, 9474, 54748, 92727, 93084,
+                548834, 1741725, 4210818, 9800817, 9926315, 24678050, 24678051, 88593477, 146511208,
+                472335975, 534494836, 912985153, 4679307774L, 32164049650L, 32164049651L, 40028394225L,
+                42678290603L, 44708635679L, 49388550606L, 82693916578L, 94204591914L, 28116440335967L,
+                4338281769391370L, 4338281769391371L, 21897142587612075L, 35641594208964132L, 35875699062250035L,
+                1517841543307505039L, 3289582984443187032L, 4498128791164624869L, 4929273885928088826L
+        };
+
         for (int i = 0; i < 20; i++) {
             DEGREE[0][i] = 0;
             DEGREE[1][i] = 1;
@@ -28,23 +37,65 @@ public class Solution {
             }
         }
 
-        System.out.println(DEGREE[0][9] + " " + DEGREE[1][15] + " " + DEGREE[3][4] + " " + DEGREE[2][10] + " " + DEGREE[9][15]);
+        byte[] zero = {0};
+        listZZ.add(zero);
+
+        for (byte c = 1; c <= 19; c++) {
+            byte numberOfDigit = c;
+            byte[] matrixMassive = new byte[numberOfDigit];
+            for (int i = numberOfDigit - 1; i >= 0; i--) {
+                matrixMassive[i] = 0;
+            }
+            while (true) {
+                byte k = 0;
+                byte countZero = 0;
+                byte countNine = 0;
+                if (matrixMassive[0] == 9 && matrixMassive[numberOfDigit - 1] == 9) {
+                    break;
+                }
+                for (int i = numberOfDigit - 1; i >= 0; i--) {
+                    if (matrixMassive[i] < 9) {
+                        matrixMassive[i]++;
+                        if (k > 0) {
+                            for (int j = numberOfDigit - 1; j > numberOfDigit - k - 1; j--) {
+                                matrixMassive[j] = matrixMassive[i];
+                            }
+                        }
+                        break;
+                    } else {
+                        k++;
+                    }
+                    if (matrixMassive[i] == 0) {
+                        countZero++;
+                    }
+
+                    if (matrixMassive[i] == 9) {
+                        countNine++;
+                    }
+
+                }
+//                System.out.println(Arrays.toString(matrixMassive));
+
+                if (countNine < 3 && countZero < 3) {
+                    listZZ.add(Arrays.copyOf(matrixMassive, matrixMassive.length));
+                }
+            }
+        }
+//        System.out.println("Array contain      " + listZZ.size() + " variants");
+
     }
 
     public static long[] getNumbers(long N) {
-        generateMatrixOfValue();
-        numberOfDigit(N);
+        ArrayList<Long> res = new ArrayList<>();
+        byte nn = numberOfDigit(N);
         int count = 0;
         for (byte[] b : listZZ) {
-            for (int ii = 0; ii < b.length; ii++) {
-//                System.out.print(b[ii] + " ");
+            if (b.length > nn) {
+                break;
             }
-            System.out.println();
             long vV = armstrongValue(b);
             if (numberOfDigit(vV) == b.length) {
-                count++;
                 ArrayList<Byte> result = toArrayListFromNumber(vV);
-                System.out.println(result.toString());
                 boolean bul = true;
                 for (byte q = 0; q < b.length; q++) {
                     if (result.get(q) != b[q]) {
@@ -53,18 +104,27 @@ public class Solution {
                     }
                 }
                 if (bul == true) {
-//                    System.out.println(b.toString());
+                    count++;
+                    if (vV < N) {
+                        res.add(vV);
+                    }
                 }
             }
         }
-        System.out.println("Count for numbers: " + count);
-        return null;
+        long [] r1 = new long[res.size()];
+        res.sort(Comparator.naturalOrder());
+        for (int i = 0; i < r1.length; i++) {
+            r1[i] = res.get(i);
+        }
+//        System.out.println("Count for numbers: " + count);
+
+        return r1;
     }
 
     public static byte numberOfDigit(long value) {
-        for (byte i = 1; i < 20; i++) {
-            if (value / 10 < DEGREE[10][i]) {
-                return (byte)(i + 2);
+        for (byte i = 1; i < 19; i++) {
+            if (value < DEGREE[10][i - 1]) {
+                return i;
             }
         }
         return 19;
@@ -94,48 +154,16 @@ public class Solution {
         return res;
     }
 
-    public static void generateMatrixOfValue() {
-        for (byte c = 1; c < 20; c++) {
-            byte numberOfDigit = c;
-            byte[] matrixMassive = new byte[numberOfDigit];
-            for (int i = numberOfDigit - 1; i >= 0; i--) {
-                matrixMassive[i] = 0;
-            }
-            while (true) {
-                byte k = 0;
-                if (matrixMassive[0] == 9 && matrixMassive[numberOfDigit - 1] == 9) {
-                    break;
-                }
-                for (int i = numberOfDigit - 1; i >= 0; i--) {
-                    if (matrixMassive[i] < 9) {
-                        matrixMassive[i]++;
-                        if (k > 0) {
-                            for (int j = numberOfDigit - 1; j > numberOfDigit - k - 1; j--) {
-                                matrixMassive[j] = matrixMassive[i];
-                            }
-                        }
-                        break;
-                    } else {
-                        k++;
-                    }
-
-                }
-                listZZ.add(matrixMassive);
-//                listZZ.add(Arrays.copyOf(matrixMassive, matrixMassive.length));
-//                System.out.println(listZZ.get(listZZ.size() - 1).toString());
-
-            }
-        }
-        System.out.println("Array contain      " + listZZ.size() + " variants");
-
-
-    }
 
     public static void main(String[] args) {
         long executionTime = System.currentTimeMillis();
-        getNumbers(9223372036854775807L);
+//        getNumbers( 9223372036854775807L);
+        getNumbers(92233720000036877L);
         executionTime = System.currentTimeMillis() - executionTime;
         System.out.println("Program execution: " + executionTime / 1000 + " sec and " + executionTime % 1000 + " millisec");
 
     }
 }
+
+
+
