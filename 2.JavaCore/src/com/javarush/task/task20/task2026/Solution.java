d87c271e2da7bd43d6b6395894f101c8e57b866d
Nikolai Borisov <nborisov@ipmks.com>
2019-06-17 10:18:18 +0300

    Update milane

--- 2.JavaCore/src/com/javarush/task/task20/task2026/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2026/Solution.java
@@ -1,6 +1,6 @@
 package com.javarush.task.task20.task2026;
 
-/* 
+/*
 Алгоритмы-прямоугольники
 */
 public class Solution {
@@ -25,6 +25,41 @@ public class Solution {
     }
 
     public static int getRectangleCount(byte[][] a) {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                if (a[i][j] == 1) {
+                    count++;
+                    int ii = i;
+                    int jj = j;
+                    a[i][j] = 3;
+                    while (true) {
+                        if (jj + 1 < a.length && a[ii][jj + 1] == 1) {
+                            jj++;
+                            a[ii][jj] = 3;
+                        } else {
+                            if (ii + 1 < a.length && a[ii + 1][j] == 1) {
+                                jj = j;
+                                ii++;
+                                a[ii][jj] = 3;
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+//        printArrays(a);
+        return count;
+    }
+
+    public static void printArrays(byte[][] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
