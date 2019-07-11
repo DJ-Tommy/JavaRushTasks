75a5773ab0ed13da66837f9fd35f2e355d89851c
Nikolai Borisov <nborisov@ipmks.com>
2019-07-11 17:50:09 +0300

    Update milane

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2207/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2207/Solution.java
@@ -1,21 +1,78 @@
 package com.javarush.task.task22.task2207;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
-/* 
+/*
 Обращенные слова
 */
 public class Solution {
     public static List<Pair> result = new LinkedList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        String fileName = reader.readLine();
+        String fileName = "d:\\44.txt";
+        reader.close();
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
+        StringBuilder stringBuilder = new StringBuilder();
 
+        while (true) {
+            String s = fileReader.readLine();
+            if (s == null) {
+                break;
+            }
+            stringBuilder.append(s + " ");
+        }
+
+        System.out.println(stringBuilder.toString());
+        ArrayList<String> stringArray = new ArrayList<>();
+        Collections.addAll(stringArray, stringBuilder.toString().split("\\s+"));
+        System.out.println(stringArray);
+
+
+        int numberOfLine = 0;
+        while (true) {
+            StringBuilder s = new StringBuilder();
+            s.append(stringArray.get(numberOfLine)).reverse();
+            boolean find = false;
+            for (int i = numberOfLine + 1; i < stringArray.size(); i++) {
+                if (s.toString().equals(stringArray.get(i))) {
+                    Pair pair = new Pair();
+                    pair.first = stringArray.get(numberOfLine);
+                    pair.second = stringArray.get(i);
+                    result.add(pair);
+                    stringArray.remove(i);
+                    stringArray.remove(numberOfLine);
+                    find = true;
+                    break;
+                }
+            }
+            if (find) {
+                continue;
+            }
+            if (++numberOfLine > stringArray.size() - 2) {
+                break;
+            }
+        }
+        for (Pair p: result) {
+            System.out.println(p.first + " " + p.second);
+            System.out.println(p);
+        }
+
+        System.out.println(stringArray);
     }
 
     public static class Pair {
         String first;
         String second;
+
+        public Pair() {
+
+        }
 
         @Override
         public boolean equals(Object o) {
