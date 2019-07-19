688e9b227db9fa0a579bbd5054388e3090298cc3
DJ-Tommy <dj_tommy@rambler.ru>
2019-07-19 13:33:09 +0300

    New Update

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/FakeModel.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FakeModel implements Model {
+
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> users = new ArrayList<>();
+        users.add(new User("Ivanov", 923l, 1));
+        users.add(new User("Petrov", 124l, 6));
+        users.add(new User("Petrov", 125l, 7));
+        users.add(new User("Sidorov", 122l, 1));
+        modelData.setUsers(users);
+    }
+}
