ae1e8e2d0906802824c6c0fd816343c3cacd90e3
DJ-Tommy <dj_tommy@rambler.ru>
2019-08-14 14:20:03 +0300

    New Update

--- 3.JavaMultithreading/src/com/javarush/task/task26/task2603/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2603/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task26.task2603;
 
-/* 
+import java.util.Comparator;
+
+/*
 Убежденному убеждать других не трудно
 */
 public class Solution {
@@ -8,4 +10,26 @@ public class Solution {
     public static void main(String[] args) {
 
     }
+
+    public static class CustomizedComparator<T> implements Comparator<T>{
+        private Comparator<T>[] comparators;
+
+        public CustomizedComparator(Comparator<T>... vararg) {
+            this.comparators = vararg;
+        }
+
+        @Override
+        public int compare(T o1, T o2) {
+            int result = 0;
+            for (Comparator<T> comparator : comparators) {
+                result = comparator.compare(o1, o2);
+                if (result == 0) {
+                    continue;
+                } else return result;
+            }
+
+            return result;
+        }
+    }
+
 }
