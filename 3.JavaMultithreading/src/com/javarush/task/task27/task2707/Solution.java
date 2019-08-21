84df86bf4a7edea35b7295238c4456e1cf9f8e49
DJ-Tommy <dj_tommy@rambler.ru>
2019-08-21 10:02:26 +0300

    New Update

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2707/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2707/Solution.java
@@ -4,17 +4,36 @@ package com.javarush.task.task27.task2707;
 Определяем порядок захвата монитора
 */
 public class Solution {
-    public void someMethodWithSynchronizedBlocks(Object obj1, Object obj2) {
-        synchronized (obj1) {
-            synchronized (obj2) {
-                System.out.println(obj1 + " " + obj2);
-            }
-        }
-    }
-
     public static boolean isLockOrderNormal(final Solution solution, final Object o1, final Object o2) throws Exception {
         //do something here
-        return false;
+
+        Thread t1 = new Thread() {
+            @Override
+            public void run() {
+                synchronized (o1) {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (o2) {
+                    }
+                }
+            }
+        };
+        Thread t2 = new Thread() {
+            @Override
+            public void run() {
+                solution.someMethodWithSynchronizedBlocks(o1, o2);
+            }
+        };
+        t1.start();
+        t2.start();
+        Thread.sleep(2000);
+        if (t2.getState() == Thread.State.BLOCKED) {
+            return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) throws Exception {
@@ -23,5 +42,13 @@ public class Solution {
         final Object o2 = new Object();
 
         System.out.println(isLockOrderNormal(solution, o1, o2));
+    }
+
+    public void someMethodWithSynchronizedBlocks(Object obj1, Object obj2) {
+        synchronized (obj1) {
+            synchronized (obj2) {
+                System.out.println(obj1 + " " + obj2);
+            }
+        }
     }
 }
