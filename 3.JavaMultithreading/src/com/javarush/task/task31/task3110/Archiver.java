1fa81052385b41421c493897ec8867b259d97bc8
DJ-Tommy <dj_tommy@rambler.ru>
2019-08-27 01:18:42 +0300

    New Update

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,4 +1,16 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
 public class Archiver {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Запроси пользователя ввести полный путь архива с клавиатуры.");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(reader.readLine()));
+        zipFileManager.createZip(Paths.get(reader.readLine()));
+        reader.close();
+    }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,4 +1,23 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 public class ZipFileManager {
+    private Path zipFile;
+
+    public ZipFileManager(Path zipFile) {
+        this.zipFile = zipFile;
+    }
+
+    public void createZip(Path source) throws Exception {
+        ZipOutputStream outputStream = (ZipOutputStream) Files.newOutputStream(zipFile);
+        ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+        outputStream.putNextEntry(zipEntry);
+        InputStream inputStream = Files.newInputStream(source);
+    }
 }
