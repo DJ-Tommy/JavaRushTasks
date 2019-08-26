ee06bd6155f65656daf7e3043db3d821a0aad8af
DJ-Tommy <dj_tommy@rambler.ru>
2019-08-27 00:48:44 +0300

    New Update

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2711/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2711/Solution.java
@@ -6,22 +6,16 @@ import java.util.concurrent.CountDownLatch;
 CountDownLatch
 */
 public class Solution {
-    private final Object lock = new Object();
-    private volatile boolean isWaitingForValue = true;
 
     CountDownLatch latch = new CountDownLatch(1);
 
     public void someMethod() throws InterruptedException {
-        synchronized (lock) {
-            while (isWaitingForValue) {
-                lock.wait();
-            }
+        latch.countDown();
+
+            latch.await();
 
             retrieveValue();
 
-            isWaitingForValue = false;
-            lock.notify();
-        }
     }
 
     void retrieveValue() {
