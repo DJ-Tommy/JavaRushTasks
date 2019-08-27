f6e17ebbf7b0e3d247fc7b2da35ab6a17f15f8b8
DJ-Tommy <dj_tommy@rambler.ru>
2019-08-27 10:56:43 +0300

    New Update

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.command.ExitCommand;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
@@ -17,5 +19,7 @@ public class Archiver {
 //        System.out.println("d:\\1.txt");
 //        zipFileManager.createZip(Paths.get("d:\\1.txt"));
         reader.close();
+        ExitCommand exitCommand = new ExitCommand();
+        exitCommand.execute();
     }
 }
