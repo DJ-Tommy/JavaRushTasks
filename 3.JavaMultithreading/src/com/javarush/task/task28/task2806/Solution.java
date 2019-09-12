cc53f5beefac1b57822f950188b0febd17089bc9
DJ-Tommy <dj_tommy@rambler.ru>
2019-09-12 20:32:54 +0300

    New Update

--- 3.JavaMultithreading/src/com/javarush/task/task28/task2806/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2806/Solution.java
@@ -11,6 +11,19 @@ public class Solution {
     public static void main(String[] args) throws InterruptedException {
         //Add your code here
 
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 10; i++) {
+            final int ii = i + 1;
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    doExpensiveOperation(ii);
+                }
+            });
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(5, TimeUnit.SECONDS);
+
         /* output example
 pool-1-thread-2, localId=2
 pool-1-thread-1, localId=1
@@ -26,6 +39,6 @@ pool-1-thread-3, localId=8
     }
 
     private static void doExpensiveOperation(int localId) {
-        System.out.println(Thread.currentThread().getName() + ", localId="+localId);
+        System.out.println(Thread.currentThread().getName() + ", localId=" + localId);
     }
 }
