0dfa945e8ca2622c955a69a5b3183724834fc3ec
DJ-Tommy <dj_tommy@rambler.ru>
2020-01-11 09:53:17 +0300

    3513 - Step 4 - 2048 (4)

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,17 +1,46 @@
 package com.javarush.task.task35.task3513;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {
     private static final int FIELD_WIDTH = 4;
     private Tile[][] gameTiles;
 
     public Model() {
+        resetGameTiles();
+    }
+
+    public void resetGameTiles() {
         gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
         for (int i = 0; i < gameTiles.length; i++) {
             for (int j = 0; j < gameTiles[0].length; j++) {
                 gameTiles[i][j] = new Tile();
             }
         }
+        addTile();
+        addTile();
     }
 
+    public void addTile() {
+        List<Tile> emptyList = getEmptyTiles();
+        if (emptyList.isEmpty()) {
+            return;
+        }
+        emptyList.get((int)(emptyList.size()*Math.random())).value = (Math.random() < 0.9 ? 2 : 4);
+    }
+
+    private List<Tile> getEmptyTiles() {
+        List<Tile> emptyTiles = new ArrayList<>();
+        for (int i = 0; i < gameTiles.length; i++) {
+            for (int j = 0; j < gameTiles[0].length; j++) {
+                if (gameTiles[i][j].isEmpty()) {
+                    emptyTiles.add(gameTiles[i][j]);
+                }
+            }
+        }
+
+        return emptyTiles;
+    }
 
 }
