5ac08812150745e1041d1f01199d738c8540d04a
DJ-Tommy <dj_tommy@rambler.ru>
2020-08-24 23:48:15 +0300

    3513 - Step 3 - 2048 (3)

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,4 +1,12 @@
 package com.javarush.task.task35.task3513;
 
 public class Model {
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+
+    public Model() {
+        for (int i = 0; i < FIELD_WIDTH * FIELD_WIDTH; i++) {
+            gameTiles[i / FIELD_WIDTH][i % FIELD_WIDTH] = new Tile();
+        }
+    }
 }
