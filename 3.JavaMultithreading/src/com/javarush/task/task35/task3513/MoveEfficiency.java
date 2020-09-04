51684e1bb5468e51b004b1cdd5cb6260b860251b
DJ-Tommy <dj_tommy@rambler.ru>
2020-09-04 22:40:08 +0300

    3513 - Step 14 - 2048 (14)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/MoveEfficiency.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task35.task3513;
+
+public class MoveEfficiency implements Move {
+    private int numberOfEmptyTiles;
+    private int score;
+    private Move move;
+
+    public MoveEfficiency(int numberOfEmptyTiles, int score, Move move) {
+        this.numberOfEmptyTiles = numberOfEmptyTiles;
+        this.score = score;
+        this.move = move;
+    }
+
+    public Move getMove() {
+        return move;
+    }
+
+    @Override
+    public void move() {
+
+    }
+}
