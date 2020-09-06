34a2f89b68f1af69d6a24c4942266bad04c0930d
DJ-Tommy <dj_tommy@rambler.ru>
2020-09-06 15:51:06 +0300

    3702 - Step 03

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task37.task3702;
 
+import com.javarush.task.task37.task3702.female.FemaleFactory;
 import com.javarush.task.task37.task3702.male.MaleFactory;
 
 public class Solution {
@@ -8,6 +9,10 @@ public class Solution {
         System.out.println(new MaleFactory().getPerson(99));
         System.out.println(new MaleFactory().getPerson(4));
         System.out.println(new MaleFactory().getPerson(15));
+
+        System.out.println(new FemaleFactory().getPerson(99));
+        System.out.println(new FemaleFactory().getPerson(4));
+        System.out.println(new FemaleFactory().getPerson(15));
 
     }
 }
